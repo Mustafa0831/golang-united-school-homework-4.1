3c8d8875ecdd67ba674d128c8f39d5e499426ebe
mus11110 <mr.mus0831@gmail.com>
2022-05-14 14:50:51 +0600

    1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,14 @@
 package reverse_string
 
+import "strings"
+
 func ReverseString(input string) (output string) {
+	reverseString := strings.Split(input, "")
+
+	for i, j := 0, len(reverseString)-1; i < j; i, j = i+1, j-1 {
+		reverseString[i], reverseString[j] = reverseString[j], reverseString[i]
+	}
+	return strings.Join(reverseString, "")
 	// solution goes here
 	return output
 }
